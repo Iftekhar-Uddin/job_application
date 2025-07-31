d8e3ece85e8dfaeb0828a1533d68d6cf95a76f71
Iftekhar Uddin <iftekharuddin720@gmail.com>
2025-07-31 23:19:58 +0600

    any comments

--- auth.ts
+++ auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import Github from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "./generated/prisma";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 export const { auth, handlers, signIn, signOut } = NextAuth({
